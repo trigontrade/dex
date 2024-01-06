ddfa344aa747616b47165296a41431fde9c4449c
trigontrade <149458283+trigontrade@users.noreply.github.com>
2024-01-06 10:40:20 +0000

    Update truffle.js

--- truffle.js
+++ truffle.js
@@ -4,9 +4,9 @@ module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
-      port: 8500,
-      network_id: "3", // Match any network id
-      gas: 7984452, // Block Gas Limit same as latest on Mainnet https://ethstats.net/
+      port: 4000,
+      network_id: "1548612888", // Match any network id
+      gas: 21000, // Block Gas Limit same as latest on Mainnet https://ethstats.net/
       gasPrice: 2000000000, // same as latest on Mainnet https://ethstats.net/
       // Mnemonic: "copy obey episode awake damp vacant protect hold wish primary travel shy"
       from: "0x7c06350cb8640a113a618004a828d3411a4f32d3"
